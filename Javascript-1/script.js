65250dc4e70acd4938f5e4005c329c5931aa1312
Deepti08 <deepu.singh8@gmail.com>
2015-01-31 21:45:43 -0500

    Javascript-1
    Assignment-1

---
+++ Javascript-1/script.js
@@ -0,0 +1,10 @@
+function sort(){
+var int1 = document.getElementById("integer1").value;
+var int2 = document.getElementById("integer2").value;
+var int3 = document.getElementById("integer3").value;
+var int4 = document.getElementById("integer4").value;
+
+var fin = Math.max(int1,int2,int3,int4);
+
+console.log('Max value is '+ fin);
+}
